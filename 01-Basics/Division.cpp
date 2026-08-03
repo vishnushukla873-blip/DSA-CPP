eb0c3301377d07a800c0ac1028b271cc196c0709
Vishnu Shukla <vishnushukla873@gmail.com>
2026-08-03 14:40:58 +0530

    Added all files of 01-Basics programs

---
+++ 01-Basics/Division.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a , b , div;
+  cout<<"Enter two numbers:";
+  cin>>a>>b;
+  div = a/b;
+  cout<<"Division of"<<a<<" and "<<b<<" is "<<div;
+  cout<<endl;
+  return 0;
+}
+
+
+
+
