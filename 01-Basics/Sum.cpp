5e66efc084ede481bc313cfb1de4f39501baf9d1
Vishnu Shukla <vishnushukla873@gmail.com>
2026-08-03 14:31:00 +0530

    Add Sum and Subtract programs

---
+++ 01-Basics/Sum.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int a , b , sum;
+   cout<<"Enter two numbers:";
+   cin>>a>>b;
+   sum = a+b;
+   cout<<"Sum of"<<a<<" and "<<b<<" is "<<sum;
+   cout<<endl;
+   return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Substract.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    cout << "Subtraction of " << a << " and " << b << " is: " << (a - b) << endl;
+    return 0;
+}
