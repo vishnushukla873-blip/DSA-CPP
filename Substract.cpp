aa544ae5c205a172dd2772246690afad16ff5839
Vishnu Shukla <vishnushukla873@gmail.com>
2026-08-04 01:24:14 +0530

    Delete Substract.cpp

--- Substract.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main() {
-    int a, b;
-    cout << "Enter two numbers: ";
-    cin >> a >> b;
-    cout << "Subtraction of " << a << " and " << b << " is: " << (a - b) << endl;
-    return 0;
-}
