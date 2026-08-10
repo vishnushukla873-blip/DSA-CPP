aa935c55aea119ee8cbc0c29d5cb091c1b665cb9
Vishnu Shukla <vishnushukla873@gmail.com>
2026-08-10 12:29:25 +0530

    Added 03-Loop all files

---
+++ 03-Loop/Fibonacci.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n ;
+    cout<<"how many terms you want :";
+    cin>>n;
+    int a = 0, b = 1 , c;
+    cout<<"Fabonacci :";
+    for(int i = 1;i<=n ; i++)
+    {
+        cout<<a<<" ";
+        c = a + b ;
+        a = b ; 
+        b = c ;
+    }
+    return 0 ;
+}
