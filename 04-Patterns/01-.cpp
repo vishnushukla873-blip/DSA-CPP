4c808a1b77ccd8d677e6893de599a0d5db4e212f
Vishnu Shukla <vishnushukla873@gmail.com>
2026-08-11 22:38:25 +0530

    Added all file of 04-Patterns

---
+++ 04-Patterns/01-.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std ;
+int main()
+{
+    int row , col ;
+    for(row=1;row<=9;row++)
+    {
+        for(col=1;col<=9;col++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+   return 0 ;
+}
---
+++ 04-Patterns/02-.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std ;
+int main()
+{
+    int row , col ; 
+    for(row = 1 ; row<=9 ; row++)
+    {
+        for(col = 1 ; col<=7 ; col++)
+        {
+            cout<<"*";
+
+        }
+        cout<<endl ;
+    }
+   return 0 ;
+
+
+}
